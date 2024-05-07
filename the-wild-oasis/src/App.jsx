3d219736394f5a62d7c5742a5865cb83c5cdb3d7
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-07 12:07:27 +0100

    Create app layouts and components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
+// import Booking from "./pages/Booking";
+// import Checkin from "./pages/Checkin";
+
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <div>
-      App
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              // <ProtectedRoute>
+              <AppLayout />
+              // </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            {/* <Route path="bookings/:bookingId" element={<Booking />} />
+          <Route path="checkin/:bookingId" element={<Checkin />} /> */}
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
       <SpeedInsights />
     </div>
   );
