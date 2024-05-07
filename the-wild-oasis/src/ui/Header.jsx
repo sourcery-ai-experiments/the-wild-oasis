c16ce0414bf3d90265f32f9bbe9bee987d57677b
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-07 12:54:30 +0100

    Update Headermenu.jsx

--- the-wild-oasis/src/ui/Header.jsx
+++ the-wild-oasis/src/ui/Header.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import HeaderMenu from "./HeaderMenu";
+import HeaderMenu from "./Headermenu";
 // import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
